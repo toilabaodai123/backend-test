6abdc81b12ff8a8348b484204af4e14a191849db
Dave Pham <dave.pham@legato.co>
2023-08-10 15:37:40 +0700

    update seed

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Store;
+use Illuminate\Support\Facades\Hash;
 
 class StoreSeeder extends Seeder
 {
@@ -14,8 +15,14 @@ class StoreSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::factory()->create();
-        Store::factory(10)->create([
+        $user = User::firstOrCreate([
+            'email' => 'guess@yahoo.com',
+        ],[
+            'name' => 'guess',
+            'password' => Hash::make('password')
+        ]);
+
+        Store::factory(1000)->create([
             'user_id' => $user->id
         ]);
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Store;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(100)->create()->each(function($user){
+            Store::factory(rand(1,20))->create([
+                'user_id' => $user->id
+            ]);
+        });
+    }
+}
